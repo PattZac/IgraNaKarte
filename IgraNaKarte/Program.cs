a84eb27db31ca6c2f68f8844dde8e38fbb4e674b
Pat <113007904+PattZac@users.noreply.github.com>
2023-05-09 17:20:58 +0200

    Add files via upload

---
+++ IgraNaKarte/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgraNaKarte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int karta = 0, zbroj = 0;
+            do
+            {
+                Console.Write("Unesite broj od 1 do 13: ");
+                karta = Convert.ToInt32(Console.ReadLine());
+                if (karta >= 1 && karta <= 13)
+                {
+                    zbroj += karta;
+                }
+            }
+            while (zbroj < 31);
+
+            if (zbroj == 31)
+            {
+                Console.WriteLine("Pobjedili ste!!!");
+            }
+            else
+            {
+                Console.WriteLine("Izgubili ste...");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
